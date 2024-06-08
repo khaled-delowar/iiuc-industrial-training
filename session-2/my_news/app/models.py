e89a60c6c117c8f7be0fae686a5f4226247655cf
Khaled Eabne Delowar <khaledibnadelowar@gmail.com>
2024-06-08 12:14:52 +0600

    update

--- session-2/my_news/app/models.py
+++ session-2/my_news/app/models.py
@@ -1,49 +1,59 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from app.database import Base
+from .database import Base
 
 class News(Base):
-    __tablename__ = 'news'
-
+    __tablename__ = "news"
     id = Column(Integer, primary_key=True, index=True)
+    # publisher_website = Column(String, index=True)
+    datetime = Column(DateTime)
     title = Column(String, index=True)
-    datetime = Column(DateTime, index=True)
-    body = Column(Text, index=True)
-    link = Column(String, unique=True, index=True)
+    body = Column(Text)
+    link = Column(String)
+    
     category_id = Column(Integer, ForeignKey('categories.id'))
     reporter_id = Column(Integer, ForeignKey('reporters.id'))
     publisher_id = Column(Integer, ForeignKey('publishers.id'))
+
     category = relationship("Category")
     reporter = relationship("Reporter")
     publisher = relationship("Publisher")
-    images = relationship("Image", back_populates="news")
+
+    # @property
+    # def category_name(self):
+    #     return self.category.name if self.category else None
+
+    # @property
+    # def reporter_name(self):
+    #     return self.reporter.name if self.reporter else None
+
+    # @property
+    # def publisher_name(self):
+    #     return self.publisher.name if self.publisher else None
 
 class Category(Base):
-    __tablename__ = 'categories'
-
+    __tablename__ = "categories"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String, unique=True)
     description = Column(String)
 
 class Reporter(Base):
-    __tablename__ = 'reporters'
-
+    __tablename__ = "reporters"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String, unique=True)
     email = Column(String, unique=True)
 
 class Publisher(Base):
-    __tablename__ = 'publishers'
-
+    __tablename__ = "publishers"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    website = Column(String, unique=True)
-    email = Column(String, unique=True, index=True)
+    name = Column(String, unique=True)
+    email = Column(String, unique=True)
+    website = Column(String, nullable=True, unique=True)
 
 class Image(Base):
-    __tablename__ = 'images'
-
+    __tablename__ = "images"
     id = Column(Integer, primary_key=True, index=True)
     news_id = Column(Integer, ForeignKey('news.id'))
     url = Column(String)
-    news = relationship("News", back_populates="images")
+
+    news = relationship("News")
